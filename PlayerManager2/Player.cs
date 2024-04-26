655176086bf8eca676fe08787e3695eb645c7e1e
Ivan <ivan.emidio@live.com.pt>
2024-04-26 10:01:17 +0100

    Playermanager2 updated

--- PlayerManager2/Player.cs
+++ PlayerManager2/Player.cs
@@ -7,6 +7,14 @@ namespace PlayerManager2
 {
     public class Player
     {
-        
+        public string Name {get;}
+        public int Score {get; set;}
+
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+
     }
 }
