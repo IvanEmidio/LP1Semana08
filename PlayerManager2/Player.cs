d4608813ef098e464c7d7c0b5e9b221558d71f1e
Ivan <ivan.emidio@live.com.pt>
2024-04-26 10:00:33 +0100

    Playermanager2

---
+++ PlayerManager2/Player.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayerManager2
+{
+    public class Player
+    {
+        
+    }
+}
