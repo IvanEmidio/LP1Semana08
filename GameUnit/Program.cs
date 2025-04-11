e66cd1743e9603f83f94d1c27ace7cf3ada2ae92
IvanEmidio <ivan.emidio@live.com.pt>
2025-04-11 11:55:53 +0100

    Added Unit class

---
+++ GameUnit/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameUnits
+{
+    public abstract class Unit 
+    //Changed to abstract because we don't know what unity it will be.
+    {
+        private int movement;
+        public virtual int Health { get; set; } 
+
+        public Unit(int movement, int health)
+        {
+            this.movement = movement;
+            Health = health;
+        }
+
+        public abstract float Cost { get; } //Read-only abstract property
+        
+        //Shows how much the unity moved
+        public void Move() => Console.WriteLine($"{movement}");
+        
+    }   
+}
